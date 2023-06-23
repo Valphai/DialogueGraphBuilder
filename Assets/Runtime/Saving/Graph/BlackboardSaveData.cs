4100ebd22f2674b0ec80ddae76d616644cf9044b
Valphai <maneqinpl@gmail.com>
2023-06-23 14:44:20 +0200

    Added SaveData for Blackboard.

---
+++ Assets/Runtime/Saving/Graph/BlackboardSaveData.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace Chocolate4.Dialogue.Runtime.Saving
+{
+    [Serializable]
+    public class BlackboardSaveData
+    {
+        public List<DialoguePropertySaveData> dialoguePropertiesSaveData;
+    }
+}
