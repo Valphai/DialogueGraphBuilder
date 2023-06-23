e59a14d960718b330afd080b4864ddcb411da18c
Valphai <maneqinpl@gmail.com>
2023-06-23 14:38:56 +0200

    Made a general list cache class using IHaveId.

---
+++ Assets/Editor/Graph/Utilities/Cache/ListCache.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Chocolate4.Edit.Graph.Utilities
+{
+    public abstract class ListCache<T> where T : IHaveId
+    {
+        protected List<T> listCache;
+
+        public IReadOnlyCollection<T> Cache => listCache;
+
+        protected abstract T GetData(string id);
+
+        public virtual bool IsCached(string id, out T cachedSaveData)
+        {
+            cachedSaveData = GetData(id);
+            return cachedSaveData != null;
+        }
+
+        public virtual bool TryCache(T data)
+        {
+            if (!IsCached(data.Id, out T cachedSituationSaveData))
+            {
+                listCache.Add(data);
+                return true;
+            }
+
+            int cachedIndex = listCache.IndexOf(cachedSituationSaveData);
+            listCache[cachedIndex] = data;
+            return false;
+        }
+
+        public virtual bool TryRemove(T situationSaveData)
+        {
+            return listCache.Remove(situationSaveData);
+        }
+    }
+}
---
+++ Assets/Editor/Graph/Utilities/Cache/SituationCache.cs
@@ -0,0 +1,36 @@
+using Chocolate4.Dialogue.Runtime.Saving;
+using System;
+using System.Collections.Generic;
+
+namespace Chocolate4.Edit.Graph.Utilities
+{
+    public class SituationCache : ListCache<SituationSaveData>
+    {
+        public event Action<string> OnSituationCached;
+
+        public SituationCache(List<SituationSaveData> newSituationSaveData = null)
+        {
+            listCache = newSituationSaveData == null
+                ? new List<SituationSaveData>()
+                : new List<SituationSaveData>(newSituationSaveData);
+        }
+
+        public override bool TryCache(SituationSaveData situationSaveData)
+        {
+            if (base.TryCache(situationSaveData))
+            {
+                OnSituationCached?.Invoke(situationSaveData.situationId);
+                return true;
+            }
+
+            return false;
+        }
+
+        protected override SituationSaveData GetData(string id)
+        {
+            return listCache.Find(
+                situation => situation.situationId == id
+            );
+        }
+    }
+}
